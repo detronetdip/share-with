f2515d096a8f9049e5cd4a24f6c67d76978efc6d
Detronetdip <ayondip2001@gmail.com>
2021-10-04 01:09:09 +0530

    commit

--- models/datamodel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const schema = new mongoose.Schema({
-  name: String,
-  email: String,
-  mobile: Number,
-  password: String,
-},{timestamps:true});
-const collection = new mongoose.model("user", schema);
-module.exports = collection;
